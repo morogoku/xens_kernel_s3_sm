bd238600812bfeaa221d78d256a09021aacb686a
xens0117 <gjstu@naver.com>
2015-02-25 18:57:18 +0900

    usb/gadget: fix compile errors

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -740,9 +740,8 @@ static int mass_storage_function_init(struct android_usb_function *f,
 {
 	struct mass_storage_function_config *config;
 	struct fsg_common *common;
-	int err;
+	int err, i;
 #ifdef CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE
-	int i;
 	unsigned int cdfs = 0;
 #endif
 	config = kzalloc(sizeof(struct mass_storage_function_config),
